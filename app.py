f3afa007609883bf5faf9611463a0d7c22c21599
Arshita Thakur <arshitathakur488@gmail.com>
2026-03-14 20:50:16 +0530

    github action

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("Hello World")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask is running!"
+
+@app.route("/about")
+def about():
+    return "This is the about page"
+
+if __name__ == "__main__":
+    app.run(debug=True)
